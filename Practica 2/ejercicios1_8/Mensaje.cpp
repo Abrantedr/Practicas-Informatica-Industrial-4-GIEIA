c5839453a630b5fc479f0af48f0a5487fb542973
Abrantedr <abrantedr@gmail.com>
2020-11-16 13:09:16 +0000

    Updated .gitignore

---
+++ Practica 2/ejercicios1_8/Mensaje.cpp
@@ -0,0 +1,5 @@
+//
+// Created by abran on 16/11/2020.
+//
+
+#include "Mensaje.hpp"
---
+++ Practica 2/ejercicios1_8/Mensaje.hpp
@@ -0,0 +1,14 @@
+//
+// Created by abran on 16/11/2020.
+//
+
+#ifndef EJERCICIOS1_8_MENSAJE_HPP
+#define EJERCICIOS1_8_MENSAJE_HPP
+
+
+class Mensaje {
+
+};
+
+
+#endif //EJERCICIOS1_8_MENSAJE_HPP
